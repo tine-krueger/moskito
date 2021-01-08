25bdedae782f729ab9115b61b60efa9f447a84c6
Tine Krüger <krueger.christ@gmail.com>
2021-01-08 12:54:03 +0100

    hints - if no campsites could be found

--- moskito-frontend/src/components/Campsitelist/CampsiteList.js
+++ moskito-frontend/src/components/Campsitelist/CampsiteList.js
@@ -14,6 +14,8 @@ export default function CampsiteList({campsites, errors}) {
     return (
         <CampList>
             {errors && <RedParagraph data-testid='error'>{errors.errors}</RedParagraph>}
+            {campsites.length === 0 && <RedParagraph>Leider konnten wir zu Deiner Anfrage keine Campingplätze finden. 
+                Vielleicht ist ja auch die Region zwischen Berlin und Hamburg was für Dich, da wirst Du bestimmt fündig!</RedParagraph>}
             {campsites.map(campsite => <CampsiteListItem key={campsite.id} campsite={campsite}/>)}
         </CampList>
     )
